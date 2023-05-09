de10dfc4781cfb7b90fa06e2727f6c245c556e24
Iryna220 <ibiriszka951@gmail.com>
2023-05-09 15:43:02 +0200

    update

---
+++ JS/second.js
@@ -0,0 +1,21 @@
+//Базові методи
+const numArr = [2, 32, 1234, 54, 323];
+let value;
+value = numArr.length;
+
+
+value = Array.isArray(numArr);
+value = numArr[2];
+numArr[2] = 12;
+value = numArr.IndexOff(32);
+value = numArr.push(100);
+value = numArr.pop();
+value = numArr.unshift(111);
+value = numArr.shift();
+value = numArr.slice(0, 2);
+value = numArr.splice(1, 0, "one", "two");
+value = numArr.reverse();
+value = numArr.concat(1, 2);
+value = numArr.join(" ");
+value = "Hello world".split(" ");
+console.log(numArr, value);
